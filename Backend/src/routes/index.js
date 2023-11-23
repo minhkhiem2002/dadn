165c3e7e811b11dfee229c4197d9c4c75d3ec5be
minhkhiem2002 <voquyen0502@gmail.com>
2023-11-23 17:20:26 +0700

    Done Equipment

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,6 +1,7 @@
 const UserRouter = require('./UserRouter')
 const FarmRouter = require('./FarmRouter')
 const DataEquipRouter = require('./DataEquipRouter')
+const ControlEquipmentRouter = require('./ControlEquipRouter')
 
 const routes = (app) => {
     // app.use('/api/user', (req,res) => {
@@ -9,6 +10,7 @@ const routes = (app) => {
     app.use('/api/user', UserRouter)
     app.use('/api/admin',FarmRouter)
     app.use('/api/dequip',DataEquipRouter)
+    app.use('/api/cequip',ControlEquipmentRouter)
 }
 
 module.exports = routes;
