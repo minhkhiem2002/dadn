a09725891e67c1781d064f1f3d5163b696aa39b0
minhkhiem2002 <106052963+minhkhiem2002@users.noreply.github.com>
2023-12-13 00:44:49 +0700

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,9 +13,14 @@ dotenv.config()
 
 const app = express();
 
+// app.use(cors({
+//     origin: ['https://dadn.000webhostapp.com/', 'http://localhost:3000'],
+//     credentials: true,
+//   }));
 app.use(cors(
     {
-        origin: 'http://localhost:3000'
+        origin: 'https://dadn.000webhostapp.com',
+        credentials: true,
     }
 ))
 
